40c3b8cc3adc4aee51d540cb924456a68e41ba63
EugeneSokolyansky <evgenys@123software.ru>
2018-06-04 15:54:27 +0300

    g-auth

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require ('passport');
+// passport config
+
+require('./config/passport')(passport);
+
+// LOAD ROUTES
+const auth = require('./routes/auth')
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
+app.get('/', (req, res) => {
+    res.send("hello,nigga");
+});
+
+// USE ROUTES
+app.use('/auth', auth)
+
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 });
 
-app.get('/',(req,res) => {
-    res.send("hello,nigga");
-})
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:"mongodb://eugene:zheka1@ds247410.mlab.com:47410/storybook_project",
+    googleClientID:'220177217167-gt2vldr78f1rl0g2ddab5agd55mch7pn.apps.googleusercontent.com',
+    googleClientSecret:'2ROoHVUENlid-oD_rRYzEV6u'
+}
