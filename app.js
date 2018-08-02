855d95771db04a3187d1d0733d2f6e76453b11fa
Eugene Sokolyansky <evgenys@123software.ru>
2018-08-02 13:43:38 +0300

    test again

--- app.js
+++ app.js
@@ -101,15 +101,17 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 });
 
-const Server = require('socket.io');
-const server = new Server(3000);
+// const Server = require('socket.io');
+// const server = new Server(3000);
+ const server = require('http').createServer(app);
+ const io = require('socket.io').listen(server)
+ server.listen(process.env.PORT || 3000);
 
-
-server.on('connection', (socket) => {
+io.on('connection', (socket) => {
     console.log('Client connected',socket.id);
     socket.on('disconnect', () => console.log('Client disconnected',socket.id));
     socket.on('chat', msg => {
-        server.emit('chat', msg);
+        io.emit('chat', msg);
         console.log('from server msg',msg);
     });
   });
